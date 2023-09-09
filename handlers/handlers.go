2649f95b99fbb40d78e8b7c24d91045884395447
Mauricio Cardoso <mauricio.cds00@gmail.com>
2023-09-09 18:16:52 -0300

    feat: add layout logic

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -10,12 +10,13 @@ func Home(response http.ResponseWriter, request *http.Request) {
 	renderTemplate(response, "home.page.html")
 }
 
+// About func
 func About(response http.ResponseWriter, request *http.Request) {
 	renderTemplate(response, "about.page.html")
 }
 
 func renderTemplate(response http.ResponseWriter, templ string) {
-	parsedTemplate, err := template.ParseFiles("./templates/" + templ)
+	parsedTemplate, err := template.ParseFiles("./templates/"+templ, "./templates/base.layout.html")
 
 	if err != nil {
 		log.Println(err)
