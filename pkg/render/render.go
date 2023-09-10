1b9d0496598ee60a9d652e2a9d3e1c08d8786f15
Mauricio Cardoso <mauricio.cds00@gmail.com>
2023-09-10 16:57:46 -0300

    feat: add basic cache logic

--- pkg/render/render.go
+++ pkg/render/render.go
@@ -1,12 +1,15 @@
 package render
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"text/template"
 )
 
-func RenderTemplate(response http.ResponseWriter, templ string) {
+var templateCache = make(map[string]*template.Template)
+
+func RenderTemplateDepr(response http.ResponseWriter, templ string) {
 	parsedTemplate, err := template.ParseFiles("./templates/"+templ, "./templates/base.layout.html")
 
 	if err != nil {
@@ -22,4 +25,51 @@ func RenderTemplate(response http.ResponseWriter, templ string) {
 
 		return
 	}
+}
+
+func RenderTemplate(response http.ResponseWriter, temp string) {
+	var template *template.Template
+	var err error
+
+	_, isInCache := templateCache[temp]
+
+	if !isInCache {
+		log.Println("Creating template and adding to cache")
+		err = createTemplateCache(temp)
+
+		if err != nil {
+			log.Println(err)
+
+			return
+		}
+	} else {
+		log.Println("Using cache template")
+	}
+
+	template = templateCache[temp]
+
+	err = template.Execute(response, nil)
+
+	if err != nil {
+		log.Println(err)
+
+		return
+	}
+}
+
+func createTemplateCache(temp string) error {
+	templates := []string{
+		fmt.Sprintf("./templates/%s", temp),
+		"./templates/base.layout.html",
+	}
+
+	templ, err := template.ParseFiles(templates...)
+
+	if err != nil {
+		return err
+	}
+
+	templateCache[temp] = templ
+
+	return nil
 }
