14fb88c36f6fdb950e08b93920e06af91ce9ac0b
Mauricio Cardoso <mauricio.cds00@gmail.com>
2023-10-24 17:11:31 -0300

    feat: change router to chi

--- cmd/routes.go
+++ cmd/routes.go
@@ -3,16 +3,19 @@ package main
 import (
 	"net/http"
 
-	"github.com/bmizerany/pat"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/mauFade/go-api/pkg/config"
 	"github.com/mauFade/go-api/pkg/handler"
 )
 
-func routes(*config.AppConfig) http.Handler {
-	mux := pat.New()
+func routes(app *config.AppConfig) http.Handler {
+	r := chi.NewRouter()
 
-	mux.Get("/", http.HandlerFunc(handler.Repo.Home))
-	mux.Get("/about", http.HandlerFunc(handler.Repo.About))
+	r.Use(middleware.Recoverer)
 
-	return mux
+	r.Get("/", http.HandlerFunc(handler.Repo.Home))
+	r.Get("/about", http.HandlerFunc(handler.Repo.About))
+
+	return r
 }
