6417c70e5c467d23c68a785481544240fdb5fb73
Mauricio Cardoso <mauricio.cds00@gmail.com>
2023-10-28 22:40:46 -0300

    feat: add sessions

--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -25,14 +25,19 @@ func NewHandlers(r *Repository) {
 }
 
 func (repo *Repository) Home(response http.ResponseWriter, request *http.Request) {
+	remoteIp := request.RemoteAddr
+	repo.App.Session.Put(request.Context(), "remote_ip", remoteIp)
+
 	render.RenderTemplate(response, "home.page.html", &model.TemplateData{})
 }
 
 func (repo *Repository) About(response http.ResponseWriter, request *http.Request) {
 	stringMap := make(map[string]string)
 
+	remoteIp := repo.App.Session.GetString(request.Context(), "remote_ip")
+
 	stringMap["hello"] = "Hello World"
-	stringMap["curse"] = "EH O FLAMENGO"
+	stringMap["remote_ip"] = remoteIp
 
 	render.RenderTemplate(response, "about.page.html", &model.TemplateData{
 		StringMap: stringMap,
